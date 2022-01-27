4f486667bf6e8f2b5e00e5987ec3166725615fcc
AnasNadifi <anas@vivoo.me>
2022-01-27 23:20:48 +0100

    initial commit

---
+++ app/src/main/java/com/nft/signdoc/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.nft.signdoc
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.fragment_signup)
+    }
+}
