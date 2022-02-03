4109fd23c37ab212168bf829cee19aa6ef721f05
AnasNadifi <anas@vivoo.me>
2022-02-03 20:18:59 +0100

    fixed shared prefs issue

--- app/src/main/java/com/nft/signdoc/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/nft/signdoc/viewmodel/UserViewModel.kt
@@ -1,11 +1,6 @@
 package com.nft.signdoc.viewmodel
 
-import android.text.*
-import android.text.method.LinkMovementMethod
-import android.text.style.ClickableSpan
-import android.view.View
-import android.widget.TextView
-import androidx.appcompat.widget.AppCompatTextView
+
 import androidx.lifecycle.ViewModel
 import com.nft.signdoc.data.preference.SharePrefs
 import com.nft.signdoc.extensions.resultFlow
@@ -15,12 +10,15 @@ import javax.inject.Inject
 
 @HiltViewModel
 class UserViewModel @Inject constructor(private val repository: Repository, sharePrefsRepository: SharePrefs) : ViewModel() {
-    var currentEmail: String = ""
-    var currentPhone: String = ""
+
+    var prefs :SharePrefs = sharePrefsRepository
+    var currentEmail: String = sharePrefsRepository.currentEmail
+    var currentPhone: String = sharePrefsRepository.currentPhone
     var usesPhone: Boolean = false
     var walletName = sharePrefsRepository.walletName
     val loginType = sharePrefsRepository.loginType
     val loggedIn = sharePrefsRepository.accessToken.isNotEmpty() || sharePrefsRepository.accessToken.isNotBlank()
+
 
     fun createUser(name: String, walletId: String, claimNFTID: String? = null) = resultFlow {
         repository.createUser(name, walletId, currentPhone, currentEmail, claimNFTID)
@@ -37,7 +35,6 @@ class UserViewModel @Inject constructor(private val repository: Repository, shar
     fun updateUser(userId: String) = resultFlow {
         repository.modifyUser(userId, currentPhone, currentEmail)
     }
-
 
 
 
